513fd0793458b66253ddd9776cbe98000bd11f88
Michael Murphy <murphy.md@gmail.com>
2013-05-20 20:05:49 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShitholEs::Application.config.secret_token = 'a2b473b497b9948c575469fa00315693f6e7cdae8afad4ade9519e08f6f29dbe7cc5d07f9819ef1895d1dd3442b40783ad36ec4e6056885da71aa0de7adb1399'
