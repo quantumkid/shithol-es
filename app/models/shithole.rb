78ae6b92a16f71ce4e13126b2938e143b2572a33
Michael Murphy <murphy.md@gmail.com>
2013-05-26 17:07:58 +0100

    Added simple validation to Shitholes model

--- app/models/shithole.rb
+++ app/models/shithole.rb
@@ -1,3 +1,9 @@
 class Shithole < ActiveRecord::Base
+  MIN_SCORE = 1
+  MAX_SCORE = 5
+  
   attr_accessible :name, :score, :lat, :lng, :imgurl, :cost  
+  
+  validates :name, :score, :cost, :lat, :lng, :presence => true
+  validates :score, :numericality => {:greater_than_or_equal_to => MIN_SCORE, :less_than_or_equal_to => MAX_SCORE}
 end
